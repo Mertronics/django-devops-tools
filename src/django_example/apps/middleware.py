7ffa66ca8874b7661be91e43a3fcc4424fd453f3
Mert Kaan <mertronics@gmail.com>
2025-08-28 14:54:58 +0300

    remove apps dir

--- src/django_example/apps/middleware.py
+++
@@ -1,8 +0,0 @@
-# src/django_example/apps/middleware.py
-
-# Middleware order is important. These will typically be added to the
-# end of the default MIDDLEWARE list in settings.py.
-PROJECT_MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
-    # 'apps.core.middleware.MyCustomMiddleware',
-]
